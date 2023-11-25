f8698b8fa997e8a3d7ffcb4eac8675524f1ac288
iamcelestino <trcelestino488@gmail.com>
2023-11-25 08:15:21 -0800

    added template engines

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
 
 
 const app = express();
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 const adminRouter = require('./routes/admin');
--- routes/admin.js
+++ routes/admin.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const path = require('path');
 
-
-
-const router = express.Router()
+const router = express.Router();
 
 
 router.get('/admin', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'views', 'product.html'));
-})
+});
 
+router.post('/admin',  (req, res) => {
+    console.log(req.body);
+    res.redirect('/');
+});
 
 module.exports = router;
